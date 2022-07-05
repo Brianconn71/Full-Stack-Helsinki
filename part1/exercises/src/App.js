abfa3fc7866414a64abaa7b626a88910af5188e1
Brian Connolly <Brian.connolly71@gmail.com>
2022-07-05 21:30:19 +0100

    made a bad job of that exercise

--- part1/exercises/src/App.js
+++ part1/exercises/src/App.js
@@ -1,3 +1,11 @@
+const Header = (props) => {
+  
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
 const Part1 = (props) => {
   
   return (
@@ -8,42 +16,60 @@ const Part1 = (props) => {
 }
 
 const Part2 = (props) => {
-  const part2 = 'Using props to pass data'
-  const exercise2 = 7
+  
   return (
     <>
-      {part2} {exercise2}
+      {props.part2} {props.exercise2}
     </>
   )
 }
 
 const Part3 = (props) => {
-  const part3 = 'State of a component'
-  const exercise3 = 14
+  
   return (
     <>
-      {part3} {exercise3}
+      {props.part3} {props.exercise3}
+    </>
+  )
+}
+const Content = (props) => {
+  
+  return (
+    <>
+      <p>
+        {props.part1}
+      </p>
+      <p>
+        {props.part2}
+      </p>
+      <p>
+        {props.part3}
+      </p>
+    </>
+  )
+}
+const Total = (props) => {
+  return(
+    <>
+      <p>Number of exercises {props.exercise1} + {props.exercise2} + {props.exercise3}</p>
     </>
   )
 }
 
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
   const exercise1 = 10
+  const exercise2 = 7
+  const exercise3 = 14
   return(
     <div>
-      <h1>{course}</h1>
-      <p>
-        <Part1 part1={part1} exercise1={exercise1}/>
-      </p>
-      <p>
-        <Part2 />
-      </p>
-      <p>
-        <Part3 />
-      </p>
-      <p>Number of exercises {}</p>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercise1={exercise1} exercise2={exercise2} exercise3={exercise3} />
     </div>
   )
 }
